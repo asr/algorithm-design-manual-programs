67d9479b74ada9a9a8cb7ee278e17066a9d132d8
Andrés Sicard-Ramírez <asr@eafit.edu.co>
2020-09-26 10:44:54 -0500

    [ matrix ] Removed unnecessary `bool.h`.

--- matrix.c
+++ matrix.c
@@ -26,10 +26,9 @@ http://www.amazon.com/exec/obidos/ASIN/0387001638/thealgorithmrepo/
 
 */
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "bool.h"
 
 /************************************************************/
 
